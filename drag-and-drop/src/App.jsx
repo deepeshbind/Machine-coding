1d23defdaeb36a4b0cdcd0e04b30bec16bdbcb0d
dufferdeepu <deepeshbind@gmail.com>
2026-01-15 01:05:11 +0530

    drag-and-drop

---
+++ drag-and-drop/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import DragAndDrop from './component/DragAndDrop'
+
+const initialData = {
+  Todo: [
+    "Design UI mockups",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway"
+  ],
+  InProgress: [
+    "Develop authentication flow",
+    "Implement responsive design"
+  ],
+  Completed: [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial version to staging"
+  ]
+};
+
+function App() {
+
+  return (
+    <div>
+      <DragAndDrop initialState={initialData} />
+    </div>
+  )
+}
+
+export default App
